56458f9fa1bb85044165260385603e4ef388e922
LucasBrisch <lucasbrisch@gmail.com>
2026-05-20 15:06:29 -0300

    adds the PWA config

--- StickerTracker2026/vite.config.js
+++ StickerTracker2026/vite.config.js
@@ -1,10 +1,34 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   plugins: [
     vue(),
     tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Sticker Tracker 2026',
+        short_name: 'Stickers 26',
+        description: 'Gerenciador Colaborativo de Figurinhas',
+        theme_color: '#09090b', /* background zinc-950 */
+        background_color: '#09090b',
+        display: 'standalone',
+        icons: [
+          {
+            src: 'icon-192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'icon-512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
   ],
 })

7a0857e07105056046f615ae73414632e49c7ecb
LucasBrisch <lucasbrisch@gmail.com>
2026-05-20 09:00:16 -0300

    adds all the supabase stuff

--- StickerTracker2026/src/App.vue
+++ StickerTracker2026/src/App.vue
@@ -1,7 +1,19 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { onMounted } from 'vue'
+import { supabase } from './lib/supabase'
+
+onMounted(async () => {
+  const { data, error } = await supabase
+    .from('stickers')
+    .select('*')
+
+  console.log(data)
+  console.log(error)
+})
 </script>
 
 <template>
-  <HelloWorld />
+  <div>
+    teste
+  </div>
 </template>
